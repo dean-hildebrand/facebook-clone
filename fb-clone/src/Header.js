39e0677839763e60d02fff48f12524fa3b892df1
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-11 14:09:38 -0400

    Imported icons

--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -1,22 +1,45 @@
-import React from 'react'
-import HeaderCSS from './Header.css'
-import SearchIcon from '@material-ui/icons/Search';
-import HomeIcon from '@material-ui/icons/Home';
+import React from "react";
+import HeaderCSS from "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
 
 function MyComponent() {
   return (
     <div class="header">
-    <div class="header-left">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/d/d4/Akash_rajoriya_Facebook_.png" alt='logo'></img>
-    <div class="header-input">
-    <SearchIcon />
-    <input type='text' placeholder='search facebook...'></input>
+      <div class="header-left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/d/d4/Akash_rajoriya_Facebook_.png"
+          alt="logo"
+        />
+        <div class="header-input">
+          <SearchIcon />
+          <input type="text" placeholder="search facebook..." />
+        </div>
+      </div>
+      <div class="header-middle">
+        <div class="header-option">
+        <HomeIcon fontsize="large"/>
+        </div>
+        <div class="header-option">
+        <FlagIcon fontsize="large"/>
+        </div>
+        <div class="header-option">
+        <SubscriptionsIcon fontsize="large"/>
+        </div>
+        <div class="header-option">
+        <StorefrontOutlinedIcon fontsize="large"/>
+        </div>
+        <div class="header-option">
+        <SupervisedUserCircleIcon fontsize="large"/>
+        </div>
+      </div>
+      <div class="header-right"></div>
     </div>
-    </div>
-    <div class="header-middle"></div>
-    <div class="header-right"></div>
-</div>
-  )
+  );
 }
 
-export default MyComponent
+export default MyComponent;
