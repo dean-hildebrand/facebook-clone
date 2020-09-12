df77dd25886cd9d8874d817b71dcb48a1cd1bca7
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-12 11:05:30 -0400

    Added rows to the sidebar with icons through props

--- fb-clone/src/components/Sidebar.js
+++ fb-clone/src/components/Sidebar.js
@@ -1,14 +1,26 @@
 import React from 'react'
-// import '../css/Sidebar.css'
+import '../css/Sidebar.css'
 import SidebarRow from '../components/SidebarRow'
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital"
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import PeopleIcon from "@material-ui/icons/People";
+import ChatIcon from "@material-ui/icons/Chat";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import ExpandMoreOutlined from "@material-ui/icons/VideoLibrary";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-<SidebarRow />
-<SidebarRow />
-<SidebarRow />
-<SidebarRow />
+{<SidebarRow src="" title="Dean Hildebrand"/>}
+<SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Information Center"/>
+<SidebarRow Icon={EmojiFlagsIcon} title="Pages"/>
+<SidebarRow Icon={PeopleIcon} title="Friends"/>
+<SidebarRow Icon={ChatIcon} title="Messenger"/>
+<SidebarRow Icon={StorefrontIcon} title="Marketplace"/>
+<SidebarRow Icon={VideoLibraryIcon} title="Videos"/>
+<SidebarRow Icon={ExpandMoreOutlined} title="Marketplace"/>
+
     </div>
   )
 }
