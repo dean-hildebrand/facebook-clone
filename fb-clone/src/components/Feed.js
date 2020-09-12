e74a3c4e6ee5c5435b233240dbf9d0e787a90a27
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-12 14:56:02 -0400

    Message sender component and form with submit

--- fb-clone/src/components/Feed.js
+++ fb-clone/src/components/Feed.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import '../css/Feed.css'
 import StoryReel from './StoryReel.js'
+import MessageSender from './MessageSender.js'
 
 function Feed() {
   return (
     <div className="feed">
-    {/* StoryReel*/}
     <StoryReel />
-    {/* Message Sender*/}
+    <MessageSender />
     </div>
   )
 }
