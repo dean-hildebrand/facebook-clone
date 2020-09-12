a9b4390c03b36ce1bbf2d8f03579a53902e0ee5d
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-12 12:45:02 -0400

    Aligned the story reel

--- fb-clone/src/components/StoryReel.js
+++ fb-clone/src/components/StoryReel.js
@@ -8,6 +8,8 @@ function StoryReel() {
     <Story image="https://i.pinimg.com/originals/55/a1/79/55a179dd73def493a38193fd3cacd278.jpg" profileSrc="https://cdn.cnn.com/cnnnext/dam/assets/200615110703-marcus-rashford-manchester-united-exlarge-169.jpg" title="Marcus Rashford"/>
     <Story image="https://i.pinimg.com/originals/55/a1/79/55a179dd73def493a38193fd3cacd278.jpg" profileSrc="https://s.hs-data.com/bilder/spieler/gross/180976.jpg" title="Paul Pogba"/>
     <Story image="https://i.pinimg.com/originals/55/a1/79/55a179dd73def493a38193fd3cacd278.jpg" profileSrc="https://tmssl.akamaized.net/images/foto/normal/david-de-gea-manchester-united-1568644672-25773.jpg" title="David De Gea"/>
+    <Story image="https://i.pinimg.com/originals/55/a1/79/55a179dd73def493a38193fd3cacd278.jpg" profileSrc="https://tmssl.akamaized.net/images/foto/normal/david-de-gea-manchester-united-1568644672-25773.jpg" title="David De Gea"/>
+    <Story image="https://i.pinimg.com/originals/55/a1/79/55a179dd73def493a38193fd3cacd278.jpg" profileSrc="https://tmssl.akamaized.net/images/foto/normal/david-de-gea-manchester-united-1568644672-25773.jpg" title="David De Gea"/>
     </div>
   )
 }
