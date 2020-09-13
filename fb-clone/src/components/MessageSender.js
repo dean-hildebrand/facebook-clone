9820b33459defdfc259c0e4f6fdd99ec990a2042
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-13 10:03:01 -0400

    Message sender form component

--- fb-clone/src/components/MessageSender.js
+++ fb-clone/src/components/MessageSender.js
@@ -1,42 +1,46 @@
-import React from 'react'
-import '../css/MessageSender.css'
-import { Avatar } from '@material-ui/core'
+import React from "react";
+import "../css/MessageSender.css";
+import { Avatar } from "@material-ui/core";
 import VideocamIcon from "@material-ui/icons/Videocam";
 import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 
-
 function MessageSender() {
-
-const handleSubmit = e => {
-  e.preventDefault()
-  console.log(e.target.value);
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
   return (
     <div className="message-sender">
-    <div className="message-sender-top">
-    <Avatar />
-    <form>
-    <input className="message-sender-input" placeholder="What's on your mind?"></input>
-    <input placeholder="image URL (Optional)"></input>
-    <button onClick={handleSubmit} type="submit">Hidden Submit</button>
-    </form>
+      <div className="message-sender-top">
+        <Avatar />
+        <form>
+          <input
+            className="message-sender-input"
+            placeholder="What's on your mind?"
+          ></input>
+          <input placeholder="image URL (Optional)"></input>
+          <button onClick={handleSubmit} type="submit">
+            Hidden Submit
+          </button>
+        </form>
+      </div>
+      <div className="message-sender-bottom">
+        <div className="message-sender-option">
+          <VideocamIcon style={{ color: "red" }} />
+          <h3>Live Video</h3>
+        </div>
+        <div className="message-sender-option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h3>Photo/Video</h3>
+        </div>
+        <div className="message-sender-option">
+          <InsertEmoticonIcon style={{ color: "orange" }} />
+          <h3>Feeling/Activity</h3>
+        </div>
+      </div>
     </div>
-    <div className="message-sender-bottom">
-    <div className="message-sender-option">
-    <VideocamIcon style={{ color: "red" }} />
-    <h3>Live Video</h3>
-    </div>
-    <div className="message-sender-option">
-    <PhotoLibraryIcon style={{ color: "green" }} />
-    <h3>Photo/Video</h3>
-    </div><div className="message-sender-option">
-    <InsertEmoticonIcon style={{ color: "orange" }} />
-    <h3>Feeling/Activity</h3>
-    </div>
-    </div>
-    </div>
-  )
+  );
 }
 
-export default MessageSender
+export default MessageSender;
