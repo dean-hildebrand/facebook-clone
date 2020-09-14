72a470c1fbb9450f7f78e272a150b75bddb6018d
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-14 16:10:26 -0400

    Set up firebase db and good login auth

--- fb-clone/src/components/firebase.js
+++ fb-clone/src/components/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB2J5V6HRIJ_BtFeW1pJVrZpn9sBkL6xUY",
@@ -9,3 +11,10 @@ const firebaseConfig = {
   appId: "1:159704301027:web:dff8f29ad564237fc6f4d6",
   measurementId: "G-CSR5EQS2EX"
 };
+const fiebaseApp = firebase.intializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider};
+export default db;
