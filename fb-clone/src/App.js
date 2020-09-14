6972d1c428b6d3fb59aeb195aecccf5be8ec0a80
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-14 15:47:47 -0400

    Styled login component

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -4,16 +4,16 @@ import Header from "./components/Header.js";
 import Sidebar from "./components/Sidebar.js";
 import Feed from "./components/Feed.js";
 import Widgets from "./components/Widgets.js";
+import Login from './components/Login.js';
 
 function App() {
   const user = null;
   return (
     <div className="app">
-    {!user ? (
-        <Login />
-    ) : (
-
-    })
+  {!user ? (
+    <Login />
+  ) : (
+    <>
       <Header />
 
       <div className="app-body">
@@ -21,8 +21,10 @@ function App() {
         <Feed />
         <Widgets />
       </div>
+      </>
+    )}
     </div>
-  );
+  )
 }
 
 export default App;
