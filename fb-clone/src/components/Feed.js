985ad9b0800c27c90f0bd313284a53f734a8975d
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-14 11:17:18 -0400

    Post component setup

--- fb-clone/src/components/Feed.js
+++ fb-clone/src/components/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../css/Feed.css'
 import StoryReel from './StoryReel.js'
+import Post from './Post.js'
 import MessageSender from './MessageSender.js'
 
 function Feed() {
@@ -8,6 +9,9 @@ function Feed() {
     <div className="feed">
     <StoryReel />
     <MessageSender />
+    <Post />
+    <Post />
+    <Post />
     </div>
   )
 }
