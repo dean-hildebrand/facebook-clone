96c2336509ad04315e72881fc4c826afb540d49f
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-14 16:40:42 -0400

    Set up provider and reducer to store user data

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -4,27 +4,27 @@ import Header from "./components/Header.js";
 import Sidebar from "./components/Sidebar.js";
 import Feed from "./components/Feed.js";
 import Widgets from "./components/Widgets.js";
-import Login from './components/Login.js';
+import Login from "./components/Login.js";
 
 function App() {
   const user = null;
   return (
     <div className="app">
-  {!user ? (
-    <Login />
-  ) : (
-    <>
-      <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
 
-      <div className="app-body">
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>
-      </>
-    )}
+          <div className="app-body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
 export default App;
