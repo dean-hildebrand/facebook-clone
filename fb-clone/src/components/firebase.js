0b1f22150dcb8138d8acb4e32b4f9ca8a33c816b
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-14 16:03:21 -0400

    Set up firebase

---
+++ fb-clone/src/components/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB2J5V6HRIJ_BtFeW1pJVrZpn9sBkL6xUY",
+  authDomain: "facebook-clone-9f12c.firebaseapp.com",
+  databaseURL: "https://facebook-clone-9f12c.firebaseio.com",
+  projectId: "facebook-clone-9f12c",
+  storageBucket: "facebook-clone-9f12c.appspot.com",
+  messagingSenderId: "159704301027",
+  appId: "1:159704301027:web:dff8f29ad564237fc6f4d6",
+  measurementId: "G-CSR5EQS2EX"
+};
