0ed08c43149ef9e6efd455493ad695b8da96ce60
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-15 13:51:48 -0400

    Changed story reel pictures and profile pictures

--- fb-clone/src/components/StoryReel.js
+++ fb-clone/src/components/StoryReel.js
@@ -5,11 +5,11 @@ import Story from './Story.js'
 function StoryReel() {
   return (
     <div className='storyReel'>
-    <Story image="https://i.pinimg.com/originals/55/a1/79/55a179dd73def493a38193fd3cacd278.jpg" profileSrc="https://cdn.cnn.com/cnnnext/dam/assets/200615110703-marcus-rashford-manchester-united-exlarge-169.jpg" title="Marcus Rashford"/>
-    <Story image="https://i.pinimg.com/originals/55/a1/79/55a179dd73def493a38193fd3cacd278.jpg" profileSrc="https://s.hs-data.com/bilder/spieler/gross/180976.jpg" title="Paul Pogba"/>
-    <Story image="https://i.pinimg.com/originals/55/a1/79/55a179dd73def493a38193fd3cacd278.jpg" profileSrc="https://tmssl.akamaized.net/images/foto/normal/david-de-gea-manchester-united-1568644672-25773.jpg" title="David De Gea"/>
-    <Story image="https://i.pinimg.com/originals/55/a1/79/55a179dd73def493a38193fd3cacd278.jpg" profileSrc="https://tmssl.akamaized.net/images/foto/normal/david-de-gea-manchester-united-1568644672-25773.jpg" title="David De Gea"/>
-    <Story image="https://i.pinimg.com/originals/55/a1/79/55a179dd73def493a38193fd3cacd278.jpg" profileSrc="https://tmssl.akamaized.net/images/foto/normal/david-de-gea-manchester-united-1568644672-25773.jpg" title="David De Gea"/>
+    <Story image="https://i.pinimg.com/originals/55/a1/79/55a179dd73def493a38193fd3cacd278.jpg" profileSrc="https://cdn.cnn.com/cnnnext/dam/assets/200615110703-marcus-rashford-manchester-united-exlarge-169.jpg" title="Dean Hildebrand"/>
+    <Story image="https://bit.ly/3hyGMgL" profileSrc="https://bit.ly/32w5lqb" title="Autumn Tully"/>
+    <Story image="https://bit.ly/3iAT9Kn" profileSrc="https://bit.ly/32xwQzO" title="Joe Thomas"/>
+    <Story image="https://bit.ly/35DlfRD" profileSrc="https://bit.ly/2FwH6Q9" title="Dominique Tansy"/>
+    <Story image="https://bit.ly/33xo5oM" profileSrc="https://bit.ly/2Fvgjna" title="Jimmy Jones"/>
     </div>
   )
 }
